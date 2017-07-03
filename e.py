1f628d10c51089f6b880b1e4876c1d46b327fcac
leonrave <alex_leon16@hotmail.com>
2017-07-03 11:54:56 -0500

    Añadiendo nuevas caracteristicas y quitando print en funciones

--- e.py
+++ e.py
@@ -1,3 +1,8 @@
-import a, b, c, d
+import a, b, c, d, f
 
-print (a.suma(3,3))
+f.presenta()
+print(a.suma(3,3))
+print(a.resta(3,3))
+print(b.multi(3,3))
+print(c.pow(3,3))
+print(d.raiz(9,2))
---
+++ f.py
@@ -0,0 +1,3 @@
+def presenta():
+
+	print("Hola, soy Alejandro...")
